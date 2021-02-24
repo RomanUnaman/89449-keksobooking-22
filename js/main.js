ff86f0185ca314b06f860a28f5117b2b6ee4bb62
RomanUnaman <68464283+RomanUnaman@users.noreply.github.com>
2021-02-24 17:39:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-const { use } = require("browser-sync");
-
-'use strict';
 function getRandomIntBetween(min,max) {
   if (min < 0 || max < 0) {
-    console.log('Аргументами функции getRandomIntBetween(min,max) могут быть только положительные числа, включая 0.')
+    console.log('Аргументами функции getRandomIntBetween(min,max) могут быть только положительные числа, включая 0.');
     return undefined;
   }
   let result = Math.round(Math.random() * (Math.abs(min - max)) + Math.min(max,min));
@@ -12,7 +9,7 @@ function getRandomIntBetween(min,max) {
 
 function getRandomFloatBetween(min,max,decimal_places) {
   if (min < 0 || max < 0 || decimal_places < 0) {
-    console.log('Аргументами функции getRandomFloatBetween(min,max,decimal_places) могут быть только положительные числа, включая 0.')
+    console.log('Аргументами функции getRandomFloatBetween(min,max,decimal_places) могут быть только положительные числа, включая 0.');
     return undefined;
   }
   let result = Math.random() * (Math.abs(min - max)) + Math.min(max,min);
